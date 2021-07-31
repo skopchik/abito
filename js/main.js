c288d3867efdd6209c93c8f2179fdaccdbd043e5
skopchik <65411869+skopchik@users.noreply.github.com>
2021-07-31 19:19:12 +0300

    Адаптивная верстка

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
+
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
-    slidesPerView: 6,
     freeMode: true,
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    }
+
 });
 var swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
@@ -11,4 +29,15 @@ var swiper2 = new Swiper(".mySwiper2", {
     thumbs: {
         swiper: swiper,
     },
+});
+
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
+});
+menuCloseBtn.addEventListener("click", () => {
+    menu.classList.remove("is-open");
 });
